79db9681c759694e3b995cca1180b94b2b32f18d
BasileBux <basile.buxtorf@ik.me>
2024-11-03 16:49:42 +0100

    updated setup relevant to home setup changes

--- hypr/scripts/monitors-toggle.sh
+++ hypr/scripts/monitors-toggle.sh
@@ -6,15 +6,14 @@
 DGPU_INTERNAL_MONITOR="eDP-1"
 INTEGRATED_INTERNAL_MONITOR="eDP-2"
 EXTERNAL_MONITOR1="DP-1"
-EXTERNAL_MONITOR2="HDMI-A-1"
 
 NUM_MONITORS=$(hyprctl monitors all | grep Monitor | wc --lines)
 NUM_MONITORS_ACTIVE=$(hyprctl monitors | grep Monitor | wc --lines)
 
 if [ $NUM_MONITORS -gt 1 ]; then
     killall hypridle
-    hyprctl keyword monitor "$DGPU_INTERNAL_MONITOR, disable"
-    hyprctl keyword monitor "$INTEGRATED_INTERNAL_MONITOR, disable"
+    hyprctl keyword monitor "$DGPU_INTERNAL_MONITOR, 2560x1600@120.00Hz, -1280x0, 2"
+    hyprctl keyword monitor "$INTEGRATED_INTERNAL_MONITOR, 2560x1600@120.00Hz, -1280x0, 2"
 
     # Monitors
     hyprctl keyword workspace "1,monitor:$EXTERNAL_MONITOR1"
@@ -22,11 +21,11 @@ if [ $NUM_MONITORS -gt 1 ]; then
     hyprctl keyword workspace "3,monitor:$EXTERNAL_MONITOR1"
     hyprctl keyword workspace "4,monitor:$EXTERNAL_MONITOR1"
     hyprctl keyword workspace "5,monitor:$EXTERNAL_MONITOR1"
-    hyprctl keyword workspace "6,monitor:$EXTERNAL_MONITOR2"
-    hyprctl keyword workspace "7,monitor:$EXTERNAL_MONITOR2"
-    hyprctl keyword workspace "8,monitor:$EXTERNAL_MONITOR2"
-    hyprctl keyword workspace "9,monitor:$EXTERNAL_MONITOR2"
-    hyprctl keyword workspace "10,monitor:$EXTERNAL_MONITOR2"
+    hyprctl keyword workspace "6,monitor:$DGPU_INTERNAL_MONITOR"
+    hyprctl keyword workspace "7,monitor:$DGPU_INTERNAL_MONITOR"
+    hyprctl keyword workspace "8,monitor:$DGPU_INTERNAL_MONITOR"
+    hyprctl keyword workspace "9,monitor:$DGPU_INTERNAL_MONITOR"
+    hyprctl keyword workspace "10,monitor:$DGPU_INTERNAL_MONITOR"
 
 fi
 # Turn off the laptop monitor if its on + more than one monitor is active
